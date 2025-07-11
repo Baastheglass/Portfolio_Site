e1f92cf3c8699bbbae87fe8fae25e4ad0f4d3c6f
Baastheglass <lightningblazer123@gmail.com>
2025-07-11 13:20:25 +0500

    initial commit, lots left

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "App">
+      <div class= "command">
+      <p class = "terminalText">baasil@DESKTOP-N4AS37I: <p class = "commandSection">type help for a list of commands</p></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
